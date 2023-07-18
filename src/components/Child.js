80d5ab1a2f02272e350af075d88a1919cc151144
VUTNUR <vutnurnethajigoud@gmail.com>
2023-07-18 17:29:40 +0530

    done

--- src/components/Child.js
+++ src/components/Child.js
@@ -18,7 +18,7 @@ const Child=({value})=>{
             {
               a && <div>
                     <h3>Modal Content</h3>
-                    <p>This is the modal content</p>
+                    <p>This is the modal content.</p>
                 </div>
             }
         </div>
